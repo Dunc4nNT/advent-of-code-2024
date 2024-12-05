28c80b2ff9a0acbe7b3270a14715069543eb38ba
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2024-12-05 23:50:50 +0100

    day 3 pt 2

--- rust/src/bin/day03.rs
+++ rust/src/bin/day03.rs
@@ -3,21 +3,35 @@ use std::time::{Duration, Instant};
 
 use anyhow::{Ok, Result};
 
-const MUL_PATTERN: &str = r"mul\((\d{1,3})\,(\d{1,3})\)";
+const MUL_PATTERN: &str = r"mul\((\d{1,3})\,(\d{1,3})\)|(do\(\))|(don\'t\(\))";
 
 fn part1(input: &str, mul_pattern: &Regex) -> Result<u32> {
     let mut total: u32 = 0;
 
     for mul_match in mul_pattern.captures_iter(input) {
-        println!("{:?}", mul_match.get(0).unwrap().start());
-        total += mul_match[1].parse::<u32>().unwrap() * mul_match[2].parse::<u32>().unwrap();
+        if mul_match.get(0).unwrap().as_str().starts_with("mul") {
+            total += mul_match[1].parse::<u32>().unwrap() * mul_match[2].parse::<u32>().unwrap();
+        }
     }
 
     Ok(total)
 }
 
 fn part2(input: &str, mul_pattern: &Regex) -> Result<u32> {
-    Ok(0)
+    let mut total: u32 = 0;
+    let mut instructions_enabled: bool = true;
+
+    for mul_match in mul_pattern.captures_iter(input) {
+        if mul_match.get(0).unwrap().as_str().starts_with("mul") && instructions_enabled {
+            total += mul_match[1].parse::<u32>().unwrap() * mul_match[2].parse::<u32>().unwrap();
+        } else if mul_match.get(0).unwrap().as_str() == "do()" {
+            instructions_enabled = true;
+        } else if mul_match.get(0).unwrap().as_str() == "don't()" {
+            instructions_enabled = false;
+        }
+    }
+
+    Ok(total)
 }
 
 #[cfg(test)]
@@ -25,6 +39,7 @@ mod tests {
     use super::*;
 
     const EXAMPLE_INPUT: &str = include_str!("../../../data/aoc/day03/example.txt");
+    const EXAMPLE_INPUT_PART2: &str = include_str!("../../../data/aoc/day03/example2.txt");
 
     #[test]
     fn test_part1() {
@@ -37,7 +52,7 @@ mod tests {
     fn test_part2() {
         let mul_pattern: Regex = Regex::new(MUL_PATTERN).unwrap();
 
-        assert_eq!(part2(EXAMPLE_INPUT, &mul_pattern).unwrap(), 0);
+        assert_eq!(part2(EXAMPLE_INPUT_PART2, &mul_pattern).unwrap(), 48);
     }
 }
 
